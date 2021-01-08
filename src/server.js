beaa1aa00cb8772a1bd7376ab24bc6428f90c463
Anna Zabolotnikova <annie.space27@gmail.com>
2021-01-08 11:46:37 +0300

    new web-server is created

---
+++ src/server.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let object = {};
+
+app.get('/', (req, res) => {
+    if (Object.keys(object).length !== 0) {
+        res.status(200).json(object);
+    } else {
+        res.status(200).json({ object: null });
+    }
+})
+
+app.post('/', (req, res) => {
+    object = JSON.parse(JSON.stringify(req.body));
+    res.status(200).json({ status: "New object is created" });
+})
+
+
+app.put('/', (req, res) => {
+    object = req.body;
+    res.status(200).json({ status: "New object is created" });
+})
+
+app.patch('/', (req, res) => {
+    if (Object.keys(object).length !== 0) {
+        object = Object.assign(object, req.body);
+        res.status(200).json({ status: "Object is updated" });
+    } else {
+        res.status(400).json({ status: "Object does not exist" });
+    }
+})
+
+app.listen(8080, 'localhost');
